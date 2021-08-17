f8901316630884b1fb8a971ff265a9f52507166e
boshtannik <boshtannik@gmail.com>
2021-08-17 17:41:37 +0300

    group2 with api - added

---
+++ back_api/group2/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from group1.models import Group1
+from rest_framework import viewsets
+from rest_framework import permissions
+from group1.serializers import Group1Serializer
+
+
+class Group1ViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows group1 to be viewed or edited.
+    """
+    queryset = Group1.objects.all()
+    serializer_class = Group1Serializer
+    permission_classes = [permissions.IsAuthenticated]  #     AllowAny
+                                                        #     IsAuthenticated
+                                                        #     IsAdminUser
+                                                        #     IsAuthenticatedOrReadOnly
+
